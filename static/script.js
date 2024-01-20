5f7a4f6c2accebc45d6a5d713e9f60f6466f7deb
yara <yara.tarek.300@gmail.com>
2024-01-20 17:40:12 +0200

    navigate to add form

--- static/script.js
+++ static/script.js
@@ -10,9 +10,8 @@ const pass_wored_new2=document.getElementById('pass_wored_new2');
 const sign_up= document.getElementById('sign_up');
 const sign_up_form=document.getElementById('sign_up_form');
 
-// // index
-const add_new_story=document.getElementById('add_new_story');
-const stories_container=document.getElementById('stories_container');
+// // home
+
 
 // // storyform
 const story_id=document.getElementById('story_id');
@@ -20,13 +19,20 @@ const story_title=document.getElementById('story_title');
 const story_content=document.getElementById('story_content');
 
 document.addEventListener('DOMContentLoaded', function () {
-
+let current_page = window.location.pathname;
+console.log(current_page);
 // login  & sign up & log out
-// -----------
-// (home)  add story 
-add_new_story.addEventListener("click",go_form)
+// -------------------------------------------------------------------------------------------
+// (home) add new story --> (story form )
+// 8888888888888888888 / -> /home
+if (current_page === '/') {
+const add_new_story=document.getElementById('add_new_story');
+// add new story --> (story form )
+function go_form(){  window.location.href = '/add_story';}
+add_new_story.addEventListener("click", go_form )
 
-// -----------
+}
+// --------------------------------------------------------------------------------------------
 // fill form 
 // ---------
 // delete & edite
@@ -36,4 +42,4 @@ add_new_story.addEventListener("click",go_form)
 // 888888888888888888888888888888 
 
 // add_new_story.addEventListener("click",post_card)
-})
+});
