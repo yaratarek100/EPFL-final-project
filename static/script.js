a1d2cdaae00e962289130c91afe5d8c13826642f
yara <yara.tarek.300@gmail.com>
2024-01-23 17:42:49 +0200

    23

--- static/script.js
+++ static/script.js
@@ -1,41 +1,29 @@
-// login 
-const user_name_box=document.getElementById('user_name_box');
-const pass_wored_box=document.getElementById('pass_wored_box');
-const log_in_button=document.getElementById('log_in_button');
-const log_in_form=document.getElementById('log_in_form');
-
-const user_name_new=document.getElementById('user_name_new');
-const pass_wored_new1=document.getElementById('pass_wored_new1');
-const pass_wored_new2=document.getElementById('pass_wored_new2');
-const sign_up= document.getElementById('sign_up');
-const sign_up_form=document.getElementById('sign_up_form');
-
-// // home
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
 let current_page = window.location.pathname;
 
-if(current_page='/'){
-    const story_cards = document.getElementsByClassName('story_card');
-    for (const story_card of story_cards) {
-    story_card.addEventListener('click', function() {
-        let card_id = story_card.id;
-        window.location.href = '/'+card_id;
-    });  }}
+    if(current_page='/'){
+        const story_cards = document.getElementsByClassName('story_card');
+        for (const story_card of story_cards) {
+        story_card.addEventListener('click', function() {
+            let card_id = story_card.id;
+            window.location.href = '/'+card_id;
+        });  
+        }}
 
     if (window.location.pathname.startsWith('/add_story')) {
-    const story_form =document.getElementById('story_form')
-    const form_title=document.getElementById('form_title');
+        const story_form =document.getElementById('story_form')
+        const form_title=document.getElementById('form_title');
 
-    form_title.innerText="Add New Story";
-    story_form.action="/post_new_story";
-   
-    }
-    function logout()
-       { localStorage.clear();}
+        form_title.innerText="Add New Story";
+        story_form.action="/post_new_story";
+    
+        }    
 
-});
+
+
+}
+
+);
+
+
--- story_app.py
+++ story_app.py
@@ -1,9 +1,12 @@
 import json
 import flask
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, jsonify
 import datetime
 app = Flask(__name__)
+
+
 json_path ='static/stories_data.json'
+users_file ='static/users_data.json'
 
 
 # classes
@@ -13,50 +16,35 @@ class story :
         self.description=description
         self.content=content
         self.datetime =date 
-
-# class user :
-#     def __init__(self,name,password) :
-#         self.name=name
-#         self.password=password
-        
-
+    # object method (obj-->dec) for json file
+    def convert_to_dectinary (self):
+        dectionary = { 
+        "title": self.title,
+        "description" :self.description,
+        "content": self.content ,
+        "date": self.datetime }
+        return dectionary    
 
 def create_story ():
     title = flask.request.args.get("story_title")
     description = flask.request.args.get("story_description")
     content= flask.request.args.get("story_content")
-# date formating
+    # date formating
     date_now=(datetime.datetime.now())
     date=str(date_now.strftime("%x"))
-    
-# object creation
+    # object creation & converting
     story_obj = story(title,description,content,date) 
+    story_dec =story_obj.convert_to_dectinary()
+    return story_dec
+    
 
-    # convert to dec for json file
-
-    story_dec = { 
-    "title": story_obj.title,
-    "description" :story_obj.description,
-    "content": story_obj.content ,
-    "date": date
-    }
-
-    return story_dec     
-
-@app.route("/")
-def home ():
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)
-    return render_template('home.html', data=data)
-
-# routing for functions
 
 @app.route("/add_story")
 def add_story ():
     empity_story={
-      "title": "",
-      "description" :"",
-      "content": ""
+    "title": "",
+    "description" :"",
+    "content": ""
     }
     return render_template('story_form.html',story= empity_story)
 
@@ -66,6 +54,18 @@ def edit_story (id):
     with open(json_path, 'r') as json_file:
         data = json.load(json_file)['stoies'][id]
     return render_template('story_form.html',story=data,id=id)
+
+@app.route("/logincheck")
+def check() :
+     with open(users_file, 'r') as json_file:
+        data = json.load(json_file)
+     return jsonify(data )  
+
+@app.route("/login")
+def go_login() :  
+     return render_template('login.html')  
+
+
 
 @app.route("/post_new_story")
 def post_new_story ():
@@ -83,7 +83,7 @@ def post_edited_story (id):
     new_story = create_story()
     with open(json_path, 'r') as json_file:
         data = json.load(json_file)
-        p=data['stoies'][id]=new_story  
+        data['stoies'][id]=new_story  
     with open(json_path, 'w') as json_file:
         json.dump(data, json_file, indent=1)
     return redirect('/')
@@ -105,6 +105,22 @@ def delet_story (id):
     with open(json_path, 'w') as json_file:
         json.dump(data, json_file, indent=1)      
     return redirect('/')
+
+
+    
+
+
+    
+
+@app.route("/")
+def home ():
+    with open(json_path, 'r') as json_file:
+        data = json.load(json_file)
+    return render_template('home.html', data=data)
+
+# routing for functions
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
