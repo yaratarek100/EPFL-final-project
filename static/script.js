6e5311caddb82cb6ddc3c90ad452e1196353c460
yara <yara.tarek.300@gmail.com>
2024-01-25 02:32:47 +0200

    integration done

--- static/script.js
+++ static/script.js
@@ -1,29 +1,84 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 let current_page = window.location.pathname;
 
-    if(current_page='/'){
-        const story_cards = document.getElementsByClassName('story_card');
-        for (const story_card of story_cards) {
-        story_card.addEventListener('click', function() {
-            let card_id = story_card.id;
-            window.location.href = '/'+card_id;
-        });  
-        }}
+// current user
+let is_user=localStorage.getItem("current_user"); 
+
+// // home page for 2 cases
+// if(current_page='/' || current_page.startsWith('/user')){
+//     // PAGE ELEMENTS
+//     const story_cards = document.getElementsByClassName('story_card');
+//     let login_button = document.getElementById('login_button');
+//     let sign_button = document.getElementById('sign_button');
+//     let add_story_button = document.getElementById('add_story_button');
+//     let logout_button = document.getElementById('logout_button');
+//     let name_p =document.getElementById("name_p");//hiden just for user name value
+
+// function user_mode (){
+//     // display name & enable  -edit -delete -add -logout
+//     // hide sign buttons
+//         sign_button.style.display = "none";
+//         login_button.style.display = "none";
+//     // display add & sign out
+//         add_story_button.style.display = "inline-block";
+//         logout_button.style.display = "inline-block";
+//         name_p.style.display = "inline-block";
+//     // can log out-->clear local storage
+//     // and redirect to home page
+//         logout_button.addEventListener("click", function(){
+//         localStorage.clear();
+//         window.location.href = '/';
+//         }) }
+    
+//     // display all story when clicked...for all cards
+//         for (const story_card of story_cards) {
+//             story_card.addEventListener('click', function() {
+//                 let card_id = story_card.id;
+//                 window.location.href = '/story/'+card_id;
+//             }); }
+ 
+// // default case
+
+//         if(current_page='/'){
+//             if (is_user==null){
+//             add_story_button.style.display = "none";
+//             logout_button.style.display = "none";  }
+//             else{
+//                 user_mode();
+//             }
+//             }
+// // only users....
+//             if (window.location.pathname.startsWith('/user')) {
+//                 // store user
+//                 const user_name=name_p.innerText
+//                 localStorage.setItem("current_user",user_name);
+//                 user_mode();  
+                
+//                 }
+// }
+// adding & editing form
 
     if (window.location.pathname.startsWith('/add_story')) {
-        const story_form =document.getElementById('story_form')
+        // change function of the edit form
+        const story_form =document.getElementById('story_form');
         const form_title=document.getElementById('form_title');
-
         form_title.innerText="Add New Story";
         story_form.action="/post_new_story";
-    
-        }    
+        }      
+
+// display all the story page
+
+ if (window.location.pathname.startsWith('/story')) {
+        let edite_button=document.getElementById('edite_button');
+        let delet_button=document.getElementById('delet_button');
+    // only signed users can edit and delete
+        if (is_user==null){
+        edite_button.style.display = "none";
+        delet_button.style.display = "none";
+        }
+        }  
 
 
 
-}
-
-);
-
+    });
 
--- story_app.py
+++ story_app.py
@@ -1,22 +1,21 @@
 import json
 import flask
-from flask import Flask, redirect, render_template, jsonify
 import datetime
+from flask import Flask, redirect, render_template, url_for
 app = Flask(__name__)
+# data_files
+stories_file ='data/stories_data.json'
+users_file ='data/users_data.json'
 
 
-json_path ='static/stories_data.json'
-users_file ='static/users_data.json'
-
-
-# classes
+# class
 class story :
     def __init__(self,title,description,content,date) :
         self.title=title
         self.description=description
         self.content=content
         self.datetime =date 
-    # object method (obj-->dec) for json file
+    #  method (obj-->dec) for json file
     def convert_to_dectinary (self):
         dectionary = { 
         "title": self.title,
@@ -24,7 +23,7 @@ class story :
         "content": self.content ,
         "date": self.datetime }
         return dectionary    
-
+    
 def create_story ():
     title = flask.request.args.get("story_title")
     description = flask.request.args.get("story_description")
@@ -37,7 +36,110 @@ def create_story ():
     story_dec =story_obj.convert_to_dectinary()
     return story_dec
     
+def creat_new_user(name,passwored):
+    return {  "name": name,"password": passwored  }
+    
+def read_file (file_path) :
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)  
+    return data
 
+def write_file (file_path, data) :
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=1)
+
+@app.route("/")
+def home ():
+    data =read_file(stories_file)
+    return render_template('home.html', data=data) 
+
+@app.route("/user/<username>")
+def user_home(username):
+    data =read_file(stories_file)  
+    return render_template('home.html', data=data,name=username) 
+
+@app.route("/login")
+def go_login() :  
+     mass ="sign in"
+     return render_template('login.html',response=mass)  
+
+@app.route("/signup")
+def go_signup() :  
+     mass ="sign up"
+     return render_template('signup.html', response=mass) 
+
+@app.route("/signupcheck")
+def su_check() :
+     user_name_new=flask.request.args.get("user_name_new")
+     pass_wored_new1=flask.request.args.get("pass_wored_new1")
+     pass_wored_new2=flask.request.args.get("pass_wored_new2")
+     if user_name_new=="no_user" :
+          mass="this name is already exist, try another one... "               
+          return render_template("signup.html", response=mass) 
+     else :
+        data =read_file(users_file)
+        users=data['users']
+        for user in users :
+            if user['name']==user_name_new :
+                mass="this name is already exist, try another one... "               
+                return render_template("signup.html", response=mass)  
+            else :
+                if pass_wored_new1==pass_wored_new2 :
+                    new_user= creat_new_user(user_name_new,pass_wored_new1)
+                    data['users'].append(new_user)
+                    write_file (users_file, data)
+                    return redirect (url_for("user_home" ,username=user_name_new))
+                else :
+                    mass="passwored is not simeller try again"
+                    return render_template("signup.html", response=mass)   
+
+
+@app.route("/logincheck")
+def logincheck() :  
+     ui_name=flask.request.args.get("ui_name")
+     ui_pass=flask.request.args.get("ui_pass") 
+     data =read_file(users_file)
+     users=data['users']
+     for user in users :
+         if user['name']==ui_name  :
+            if user['password']==ui_pass :
+                return redirect (url_for("user_home", username=ui_name) )
+            else :
+                mass="password isn't correct"
+                return render_template("/login.html", response=mass) 
+         else :
+            mass="user name isn't correct"
+            return render_template("/login.html", response=mass)  
+        
+@app.route("/post_new_story")
+def post_new_story ():
+    new_story = create_story()
+    data =read_file(stories_file)
+    data['stoies'].append(new_story)
+    write_file (stories_file, data)
+    return redirect('/')  
+
+@app.route("/<int:id>/post_edited_story")
+def post_edited_story (id):
+    new_story = create_story()
+    data =read_file(stories_file)
+    data['stoies'][id]=new_story  
+    write_file(stories_file,data)
+    return redirect('/')
+
+@app.route("/story/<int:id>")
+def display_story (id):
+    data =read_file(stories_file)
+    story=data['stoies'][id]
+    return render_template('display.html', story=story,id=id)
+
+@app.route("/<int:id>/delet_story")
+def delet_story (id):  
+    data =read_file(stories_file)
+    stories_data =data['stoies']
+    del stories_data[id]
+    write_file(stories_file,data)    
+    return redirect('/')
 
 @app.route("/add_story")
 def add_story ():
@@ -48,79 +150,11 @@ def add_story ():
     }
     return render_template('story_form.html',story= empity_story)
 
-@app.route("/<id>/edit_story")
+@app.route("/<int:id>/edit_story")
 def edit_story (id):
-    id=int(id)
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)['stoies'][id]
-    return render_template('story_form.html',story=data,id=id)
-
-@app.route("/logincheck")
-def check() :
-     with open(users_file, 'r') as json_file:
-        data = json.load(json_file)
-     return jsonify(data )  
-
-@app.route("/login")
-def go_login() :  
-     return render_template('login.html')  
-
-
-
-@app.route("/post_new_story")
-def post_new_story ():
-    new_story = create_story()
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)
-        data['stoies'].append(new_story)
-    with open(json_path, 'w') as json_file:
-        json.dump(data, json_file, indent=1)
-    return redirect('/')  
-
-@app.route("/<id>/post_edited_story")
-def post_edited_story (id):
-    id=int(id)
-    new_story = create_story()
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)
-        data['stoies'][id]=new_story  
-    with open(json_path, 'w') as json_file:
-        json.dump(data, json_file, indent=1)
-    return redirect('/')
-
-@app.route("/<id>")
-def display_story (id):    
-    id=int(id)
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)['stoies'][id]
-    return render_template('prsonal.html', story=data,id=id)
-
-@app.route("/<id>/delet_story")
-def delet_story (id):    
-    id=int(id)
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)
-        stories_data =data['stoies']
-        del stories_data[id]
-    with open(json_path, 'w') as json_file:
-        json.dump(data, json_file, indent=1)      
-    return redirect('/')
-
-
-    
-
-
-    
-
-@app.route("/")
-def home ():
-    with open(json_path, 'r') as json_file:
-        data = json.load(json_file)
-    return render_template('home.html', data=data)
-
-# routing for functions
-
-
+    data =read_file(stories_file)
+    story=data['stoies'][id]
+    return render_template('story_form.html',story=story,id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
