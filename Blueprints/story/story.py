ba75501738ab6c85d754fe4082f3a49d6a4f2238
yaratarek100 <yara.tarek.300@gmail.com>
2024-03-04 04:21:54 +0200

    stories

--- Blueprints/story/story.py
+++ Blueprints/story/story.py
@@ -7,17 +7,17 @@ story_bp= Blueprint( "story" ,__name__, template_folder = "templates")
 @story_bp.route("/story/<int:id>")
 def display_story (id):
     data =read_file(stories_file)
-    srories_num =len(data['stoies'])
+    srories_num =len(data['stories'])
     if(id>= srories_num) :
         abort(404)
     else :
-        story=data['stoies'][id]
+        story=data['stories'][id]
         return render_template('display.html', story=story,id=id)
         
 @story_bp.route("/<int:id>/delet_story")
 def delet_story (id):  
     data =read_file(stories_file)
-    stories_data =data['stoies']
+    stories_data =data['stories']
     del stories_data[id]
     write_file(stories_file,data)    
     return redirect('/')
@@ -31,21 +31,21 @@ def add_story ():
 def post_new_story ():
     new_story = create_story()
     data =read_file(stories_file)
-    data['stoies'].append(new_story)
+    data['stories'].append(new_story)
     write_file (stories_file, data)
     return redirect('/')  
 
 @story_bp.route("/<int:id>/edit_story")
 def edit_story (id):
     data =read_file(stories_file)
-    story=data['stoies'][id]
+    story=data['stories'][id]
     return render_template('story_form.html',story=story,id=id)
 
 @story_bp.route("/<int:id>/post_edited_story")
 def post_edited_story (id):
     new_story = create_story()
     data =read_file(stories_file)
-    data['stoies'][id]=new_story  
+    data['stories'][id]=new_story  
     write_file(stories_file,data)
     return redirect('/')
 
